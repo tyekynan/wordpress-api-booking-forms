83d1a152ab48bc649fce343abdba6800d8c55fa0
tyekynan <78494359+tyekynan@users.noreply.github.com>
2023-10-10 15:27:02 -0400

    Update

--- functions.php
+++ functions.php
@@ -1,10 +1,14 @@
 <?php
-$object_type = 'post';
-$meta_args = array(
-    'type'         => 'string',
-    'description'  => 'Reservation status assigned to reservation',
-    'single'       => true,
-    'show_in_rest' => true,
-);
+//Located in the functions.php file of my theme folder.
+add_action('rest_api_init', function() {
 
-register_meta( $object_type, 'reservation-status', $meta_args );
+    $object_type = 'post';
+    $meta_args = array(
+        'type'         => 'string',
+        'description'  => 'Reservation status assigned to reservation',
+        'single'       => true,
+        'show_in_rest' => true,
+    );
+    
+    register_meta( $object_type, 'reservation-status', $meta_args );
+    });
